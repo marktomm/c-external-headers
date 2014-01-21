17798eb7aac66290cfaf3dcef594cfe343020734
Mark Tomm <marktomm90@gmail.com>
2014-01-21 10:18:04 +0200

    write output to file

--- gdbinit.sh
+++ gdbinit.sh
@@ -2,59 +2,70 @@
 
 cpp_compiler_version=`g++ -dumpversion`
 
-gdbskip=tmp
-gdbinit=~/.gdbinit
+gdbskip=~/.gdbskip
+gdbinit=~/.gdbinit  	# unused
 
 includedir=/usr/include
-srcdir=/home/mark/Projects/telem-gw6/Gateway6/source/
+srcdir=/home/mark/Projects/clone/develop/gw/Gateway6/source/
 stdcdir=$includedir
 boostdir=$includedir
 linuxgnu=$includedir/x86_64-linux-gnu
 linuxdir=$includedir/linux
 log4cxx=$includedir/log4cxx
 cppdir=$includedir/c++/$cpp_compiler_version
+custom_dir1=/home/mark/Projects/clone/develop/gw/Gateway6/source/libcpp
+
+rm -f $gdbskip
 
 for line in $(grep --include=*.{h,cpp} -r --no-filename '#include.*\(\([<|"]boost\).\|<\)' $srcdir | sed 's/.*include.*[<\|"]\(.*\)[>\|"].*/\1/' | sort -u); do
 	[[ $line == *.h ]] && [[ $line != */* ]] && {
 		findres=`find $stdcdir -maxdepth 1 -name $line -print`
-		[[ ! -z "$findres" ]] && echo "$findres" && continue
+		[[ ! -z "$findres" ]] && echo "$findres" >> $gdbskip && continue
 	}
 	[[ $line == sys/* ]] && {
 # 		findres=`basename $line`
 		findres=`find $linuxgnu/sys/ -name "$(basename $line)" -print`
-		[[ ! -z "$findres" ]] && echo "$findres" && continue
+		[[ ! -z "$findres" ]] && echo "$findres" >> $gdbskip && continue
 	}
 	[[ $line == asm/* ]] && {
 # 		findres=`basename $line`
 		findres=`find $linuxgnu/asm/ -name "$(basename $line)" -print`
-		[[ ! -z "$findres" ]] && echo "$findres" && continue
+		[[ ! -z "$findres" ]] && echo "$findres" >> $gdbskip && continue
+	}
+	[[ $line == *.h ]] && [[ $line != */* ]] && {
+		findres=`find $linuxgnu -name "$line" -print`
+		[[ ! -z "$findres" ]] && echo "$findres" >> $gdbskip && continue
 	}
 	[[ $line == *.h ]] && [[ $line != */* ]] && {
 		findres=`find $linuxdir -name "$line" -print`
-		[[ ! -z "$findres" ]] && echo "$findres" && continue
+		[[ ! -z "$findres" ]] && echo "$findres" >> $gdbskip && continue
 	}
 	
 # 	Other Libs
 	[[ $line == log4cxx* ]] && {
 # 		line=`basename $line`
 		findres=`find $log4cxx -name "$(basename $line)" -print`
-		[[ ! -z $findres ]] && echo "$findres" && continue
+		[[ ! -z $findres ]] && echo "$findres" >> $gdbskip && continue
 	}
 	[[ $line == boost* ]] && {
-		echo "$boostdir/$line" && continue
+		echo "$boostdir/$line" >> $gdbskip && continue
 	}
 # 	Other End
 
 #	STL
 	[[ $line != */* ]] && {
 		findres=`find $cppdir -name $line -print` 
-		[[ ! -z $findres ]] && echo "$findres	" && continue
+		[[ ! -z $findres ]] && echo "$findres" >> $gdbskip && continue
 	}
 # 	STL End
 
 # 	Custom
 	[[ $line == microhttpd.h ]] && {
-		echo "/usr/local/include/microhttpd.h" && continue
+		echo "/usr/local/include/microhttpd.h" >> $gdbskip && continue
+	}
+	[[ $line == libcpp* ]] && {
+		findres=`find $custom_dir1 -name "$(basename $line)" -print`
+		[[ ! -z $findres ]] && echo "$findres" >> $gdbskip && continue
 	}
 	
 # 	There were some more header files, but they were irrelevant in my current situation, so ignore
